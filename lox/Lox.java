21b13bbdff668685bad9574fb92942c394578b72
fargelus <ddraudred@gmail.com>
2023-07-10 10:13:02 +0400

    Added tokens scanner

--- lox/Lox.java
+++ lox/Lox.java
@@ -57,7 +57,7 @@ public class Lox {
     }
 
     private static void report(int line, String where, String message) {
-        Systen.err.println(
+        System.err.println(
             "[line " + line + "] Error" + where + ": " + message
         );
         hadError = true;
